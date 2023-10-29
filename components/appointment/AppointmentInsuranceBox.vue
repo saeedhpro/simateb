42b083cdeb94c4718484d0ed6d6120c81adf58b4
Saeed Heydari <saeedhpro@gmail.com>
2023-10-29 17:04:32 +0330

    Fix

--- components/appointment/AppointmentInsuranceBox.vue
+++ components/appointment/AppointmentInsuranceBox.vue
@@ -146,7 +146,7 @@ export default {
       return this.total * this.selectedInsurance.percentage / 100
     },
     patient_price() {
-      return this.total - this.insurance_price - this.discount_price
+      return this.total - this.insurance_price
     }
   }
 }
